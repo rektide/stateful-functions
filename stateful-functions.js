596d55f86d705a798715a859b8e2c170c2a8ede0
rektide de la faye <rektide@voodoowarez.com>
2018-10-27 16:14:41 -0400

    implement useEffect & useState

--- stateful-functions.js
+++ stateful-functions.js
@@ -1,20 +1,43 @@
-export function useState( val){
-	let _val= val
-	function setValue( val){
-		val= _val
+const effect= Symbol.for("stateful-function:effect")
+function raiseEffect(fn){
+	const effects= fn[ effect]
+	if( !effects){
+		retrun
 	}
-	return [ _val, setValue ]
+	effects.forEach( effect=> effect( fn))
 }
 
-export function useEffect( fn){
-	
+const _state= new WeakMap()
+export function useState( fn, val){
+	// retrieve existing
+	if( _state.has( fn)){
+		return  state.get( fn)
+	}
+
+	// state
+	let _val= val
+	function setValue( val){
+		const effected= val!== _val
+		val= _val
+		if(effected){
+			raiseEffect(fn)
+		}
+	}
+
+	const state= [ _val, setvalue]
+	_state.set( state)
+	return state
+}
+
+export function useEffect( fn, cb){
+	const effects= fn[ effect]|| (fn[ effect]= [])
+	effects.push( fn)
 }
 
 export function Context( val){
 	return { context: val }
 }
 
-export function useContext( ctx){
+export function useContext( fn, ctx){
 	
 }
-
